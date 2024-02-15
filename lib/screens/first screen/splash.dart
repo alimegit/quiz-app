b34cb3d97cb9dc3c3710e3d488bc3d72df7ebfe6
Ali <alisherurinov@68@gmail.com>
2024-02-15 12:04:54 +0500

    o

--- lib/screens/first screen/splash.dart
+++ lib/screens/first screen/splash.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../../utils/colors/app_colors.dart';
+import '../../utils/images/app_images.dart';
+
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text("Ali",style: TextStyle(color: AppColors.white),),
-      ),
-      body: Center(
-        child: Text("Ali",style: TextStyle(color: AppColors.c_1A72DD),),
-      ),
-    );
+        backgroundColor: AppColors.c_6A5AE0,
+        body: Stack(
+          children: [
+            Positioned(
+              top: 100.h,
+                child: SvgPicture.asset(AppImages.oval)),
+            Center(
+              child: Image.asset(AppImages.splash),
+            ),
+          ],
+        ));
   }
 }
--- lib/utils/colors/app_colors.dart
+++ lib/utils/colors/app_colors.dart
@@ -5,18 +5,6 @@ class AppColors {
   static const Color white = Colors.white;
   static const Color transparent = Colors.transparent;
 
-  static const Color c_53B175 = Color(0xFF53B175);
-  static const Color c_FCFCFC = Color(0xFFFCFCFC);
-  static const Color c_181725 = Color(0xFF181725);
-  static const Color c_F2F3F2 = Color(0xFFF2F3F2);
-  static const Color c_000000 = Color(0xFF000000);
-  static const Color c_F8A44C = Color(0xFFF8A44C);
-  static const Color c_F7A593 = Color(0xFFF7A593);
-  static const Color c_D3B0E0 = Color(0xFFD3B0E0);
-  static const Color c_FDE598 = Color(0xFFFDE598);
-  static const Color c_B7DFF5 = Color(0xFFB7DFF5);
-  static const Color c_555E58 = Color(0xFF555E58);
-  static const Color c_7C7C7C = Color(0xFF7C7C7C);
-  static const Color c_1A72DD = Color(0xFF1A72DD);
+  static const Color c_6A5AE0 = Color(0xFF6A5AE0);
 
 }
